d6ec025fa14ca604cb68c80f73a29b1dee6c20e1
martin-fitger <57917288+martin-fitger@users.noreply.github.com>
2022-11-28 09:09:47 +0100

    Changed license to GNU LGPL

--- pstalgo/include/pstalgo/BFS.h
+++ pstalgo/include/pstalgo/BFS.h
@@ -4,18 +4,18 @@ Copyright 2019 Meta Berghauser Pont
 This file is part of PST.
 
 PST is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
+it under the terms of the GNU Lesser General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version. The GNU General Public License
+(at your option) any later version. The GNU Lesser General Public License
 is intended to guarantee your freedom to share and change all versions
 of a program--to make sure it remains free software for all its users.
 
 PST is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU Lesser General Public License for more details.
 
-You should have received a copy of the GNU General Public License
+You should have received a copy of the GNU Lesser General Public License
 along with PST. If not, see <http://www.gnu.org/licenses/>.
 */
 
