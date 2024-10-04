cf102411b4be5f67448c214a091149aa1368325d
Martin Fitger <martin.fitger@gmail.com>
2024-10-04 14:34:25 +0200

    pstalgo: VS2022 compile fix.

--- pstalgo/src/geometry/RegionPoints.cpp
+++ pstalgo/src/geometry/RegionPoints.cpp
@@ -19,6 +19,7 @@ You should have received a copy of the GNU Lesser General Public License
 along with PST. If not, see <http://www.gnu.org/licenses/>.
 */
 
+#include <stdexcept>
 #include <vector>
 #include <pstalgo/Vec2.h>
 
